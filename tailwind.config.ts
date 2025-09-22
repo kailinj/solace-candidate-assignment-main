6c07403cf159408d5cbbb6ad122e84eca12de47e
Kailin Johnson <kailin.johnson@gmail.com>
2025-09-22 11:52:14 -0500

    chore: update tailwind, install heroui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const { heroui } = require("@heroui/react");
 
 const config: Config = {
   content: [
@@ -6,6 +7,8 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
+  plugins: [heroui()],
   theme: {
     extend: {
       backgroundImage: {
@@ -15,6 +18,5 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
 };
 export default config;
